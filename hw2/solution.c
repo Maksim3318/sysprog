800dbdd9fe8c13dca31ed79ad97f32d24adb7e6f
Maksim <maksim.selin@icloud.com>
2024-02-25 15:49:54 +0300

    execvp + pipes (using wait())

--- hw2/solution.c
+++ hw2/solution.c
@@ -3,50 +3,138 @@
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-static void
-execute_command_line(const struct command_line *line)
-{
-	/* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
-
-	assert(line != NULL);
-	printf("================================\n");
-	printf("Command line:\n");
-	printf("Is background: %d\n", (int)line->is_background);
-	printf("Output: ");
-	if (line->out_type == OUTPUT_TYPE_STDOUT) {
-		printf("stdout\n");
-	} else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-		printf("new file - \"%s\"\n", line->out_file);
-	} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-		printf("append file - \"%s\"\n", line->out_file);
-	} else {
-		assert(false);
+char **build_argv(const struct command *cmd) {
+	char **argv = malloc((2 + cmd->arg_count) * sizeof(char *));
+	argv[0] = cmd->exe;
+	for (uint32_t i = 0; i < cmd->arg_count; ++i) {
+		argv[i + 1] = cmd->args[i];
 	}
-	printf("Expressions:\n");
-	const struct expr *e = line->head;
-	while (e != NULL) {
-		if (e->type == EXPR_TYPE_COMMAND) {
-			printf("\tCommand: %s", e->cmd.exe);
-			for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-				printf(" %s", e->cmd.args[i]);
-			printf("\n");
-		} else if (e->type == EXPR_TYPE_PIPE) {
-			printf("\tPIPE\n");
-		} else if (e->type == EXPR_TYPE_AND) {
-			printf("\tAND\n");
-		} else if (e->type == EXPR_TYPE_OR) {
-			printf("\tOR\n");
-		} else {
-			assert(false);
-		}
-		e = e->next;
-	}
+	argv[cmd->arg_count + 1] = NULL;
+	return argv;
 }
 
-int
-main(void)
-{
+static void execute_command_line(const struct command_line *line) {
+	const struct expr *e = line->head;
+
+	if (e->type == EXPR_TYPE_COMMAND 
+		&& !strncmp(e->cmd.exe, "exit", 6)
+		&& e->next == NULL) {
+			int ret = 0;
+			if (e->cmd.arg_count) {
+				ret = atoi(e->cmd.args[0]);
+			}
+			exit(ret);
+	}
+	int dup_stdout = dup(STDOUT_FILENO);
+	int dup_stdin = dup(STDIN_FILENO);
+	int file = dup(STDOUT_FILENO);
+	
+	if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
+		file = open(line->out_file, O_RDWR | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
+		dup2(file, STDOUT_FILENO);
+	} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
+		file = open(line->out_file, O_RDWR | O_CREAT | O_APPEND | O_CLOEXEC, 0644);
+		dup2(file, STDOUT_FILENO);
+	}
+
+	// fprintf(stderr, "dup_stdin = %d\n", dup_stdin);
+	// fprintf(stderr, "dup_stdout = %d\n", dup_stdout);
+	// fprintf(stderr, "file = %d\n", file);
+
+	bool in_pipe_left = false;
+	bool in_pipe_right = false;
+	int fd_l[2] = {-1, -1};
+	int fd_r[2] = {-1, -1};
+
+	while (e != NULL) {
+		if (e->type == EXPR_TYPE_COMMAND) {
+			// fprintf(stderr, "====================\n");
+			if (!strncmp(e->cmd.exe, "cd", 4)) {
+				chdir(*e->cmd.args);
+				e = e->next;
+				continue;
+			}
+			if (e->next && e->next->type == EXPR_TYPE_PIPE) {
+				in_pipe_left = true;
+			} else {
+				in_pipe_left = false;
+			}
+			if (in_pipe_right) {
+				// fprintf(stderr, "RIGHT FROM PIPE\n");
+				dup2(fd_r[0], STDIN_FILENO);
+				// fprintf(stderr, "stdin = %d\n", fd_r[0]);
+				// fprintf(stderr, "%d closed\n", fd_l[1]);
+				close(fd_l[1]);
+			} else {
+				// fprintf(stderr, "NOT RIGHT FROM PIPE\n");
+				dup2(dup_stdin, STDIN_FILENO);
+				// fprintf(stderr, "stdin = %d\n", dup_stdin);
+				// fprintf(stderr, "%d closed\n", fd_r[0]);
+				close(fd_r[0]);
+			}
+			if (in_pipe_left) {
+				// fprintf(stderr, "LEFT FROM PIPE\n");
+				pipe(fd_l);
+				// fprintf(stderr, "pipe() in = %d, out = %d\n", fd_l[0], fd_l[1]);
+				dup2(fd_l[1], STDOUT_FILENO);
+				// fprintf(stderr, "stdout = %d\n", fd_l[1]);
+			} else {
+				// fprintf(stderr, "NOT LEFT FROM PIPE\n");
+				dup2(file, fd_r[1]);
+				dup2(file, STDOUT_FILENO);
+				// fprintf(stderr, "stdout = %d\n", file);
+			}
+			
+			// fprintf(stderr, "FORK CALL\n");
+			pid_t pid = fork();
+			if (pid == 0) {
+				// fprintf(stderr, "EXEC CALL %s\n", e->cmd.exe);
+				char **argv = build_argv(&e->cmd);
+				execvp(e->cmd.exe, argv);
+			} else if (pid > 0) {
+				// fprintf(stderr, "WAIT CALL\n");
+				wait(NULL);
+				// fprintf(stderr, "WAIT END\n");
+			}
+		} else if (e->type == EXPR_TYPE_PIPE) {
+			in_pipe_right = true;
+			if (fd_r[0]) {
+				close (fd_r[0]);
+			}
+			if (fd_r[1]) {
+				close (fd_r[1]);
+			}
+			fd_r[0] = dup(fd_l[0]);
+			// fprintf(stderr, "pipe in_copy = %d", fd_r[0]);
+			// fprintf(stderr, "%d closed\n", fd_l[0]);
+			// fprintf(stderr, "%d closed\n", fd_l[1]);
+			close(fd_l[0]);
+			close(fd_l[1]);
+			fd_l[0] = -1;
+			fd_l[1] = -1;
+		} else if (e->type == EXPR_TYPE_AND) {
+
+		} else if (e->type == EXPR_TYPE_OR) {
+		
+		}
+		e = e->next;
+		dup2(dup_stdout, STDOUT_FILENO);
+		dup2(dup_stdin, STDIN_FILENO);
+	}
+	dup2(dup_stdin, STDIN_FILENO);
+	dup2(dup_stdout, STDOUT_FILENO);
+	close(dup_stdin);
+	close(dup_stdout);
+}
+
+int main(void) {
 	const size_t buf_size = 1024;
 	char buf[buf_size];
 	int rc;
@@ -54,6 +142,7 @@ main(void)
 	while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
 		parser_feed(p, buf, rc);
 		struct command_line *line = NULL;
+		// fprintf(stderr, "here\n");
 		while (true) {
 			enum parser_error err = parser_pop_next(p, &line);
 			if (err == PARSER_ERR_NONE && line == NULL)
